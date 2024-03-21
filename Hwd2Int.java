f08b6c488279a96b915d7788ed4ca225f7f84325
rlatjsdn71 <rlatjsdn0701@gmail.com>
2024-03-21 14:29:02 +0900

    Hwd2Int.java 추가 Hwd2Int.java : "하나", "일", "첫째" 입력 시 1 출력, "둘", "이", "둘째" 입력 시 2출력 그 외 는 "없음" 출력

---
+++ Hwd2Int.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hwd2Int {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("한글 입력 :");
+        String str = scan.next();
+        switch(str){
+            case "하나":
+            case "일":
+            case "첫째":
+                System.out.println(1);
+                break;
+            case "둘":
+            case "이":
+            case "둘째":
+                System.out.println(2);
+                break;
+            default:
+                System.out.println("없음");
+        }
+    }
+}
