108e634f28229902e7cf5872ed35af108fb5d2a0
rlatjsdn71 <rlatjsdn0701@gmail.com>
2024-03-21 14:21:38 +0900

    Int2Hwd.java 추가 Int2Hwd.java : 1입력 시 "하나", 2입력 시 "둘", 그 외 "없음" 출력

---
+++ Int2Hwd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Int2Hwd {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수를 입력하세요: ");
+        int num = scan.nextInt();
+        switch(num){
+            case 1:
+                System.out.println("하나");
+                break;
+            case 2:
+                System.out.println("둘");
+                break;
+            default: System.out.println("없음");
+        }
+    }
+}
