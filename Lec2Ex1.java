a520d89863f42bcf6934d5ef9d8e38fe04aca372
rlatjsdn71 <rlatjsdn0701@gmail.com>
2024-03-21 14:08:39 +0900

    Lec2Ex1.java 추가 Lec2Ex1.java : 2개의 정수를 입력받아 더 큰 수를 출력

---
+++ Lec2Ex1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lec2Ex1 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수1을 입력하세요 :");
+        int num1 = scan.nextInt();
+        System.out.print("정수2을 입력하세요 :");
+        int num2 = scan.nextInt();
+        if(num1==num2) {
+            System.out.println("같은 수 입니다.");
+            return;
+        }
+        System.out.print("더 큰 수는 ");
+        if(num1>num2) System.out.println(num1+" 입니다.");
+        else System.out.println(num2+" 입니다.");
+    }
+}
